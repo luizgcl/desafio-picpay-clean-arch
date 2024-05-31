f5acc18fc7a70cf383cc086cedb2643b79de71a1
luiizgcl <luiizgcl@gmail.com>
2024-05-31 00:45:35 -0300

    feat(core): add email exception

---
+++ core/src/main/java/br/com/luizgcl/core/exception/EmailException.java
@@ -0,0 +1,9 @@
+package br.com.luizgcl.core.exception;
+
+public class EmailException extends BaseException {
+
+    public EmailException(String message, String code) {
+        super(message, code);
+    }
+
+}
